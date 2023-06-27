339a65a592288fb71a863efe3ff50587af08b73d
Marek Ciupak <6394528+marekciupak@users.noreply.github.com>
2023-06-27 21:25:50 +0200

    Add redux

--- assets/ts/index.tsx
+++ assets/ts/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import {store} from "./app/store";
+import {Provider} from "react-redux";
 import HelloWorld from "./components/HelloWorld";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element.");
 const root = createRoot(rootElement);
 
-root.render(<HelloWorld />);
+root.render(
+  <Provider store={store}>
+    <HelloWorld />
+  </Provider>
+);
